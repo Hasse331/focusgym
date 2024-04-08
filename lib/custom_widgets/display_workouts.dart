7c7dc391315bb61be3e8a50da7c415c2a58a655d
Hasse <imberg.hasse@pm.me>
2024-04-08 19:48:10 +0300

    removed unnecessary margin

--- lib/custom_widgets/display_workouts.dart
+++ lib/custom_widgets/display_workouts.dart
@@ -14,25 +14,22 @@ class DisplayWorkouts extends StatelessWidget {
     final isSelectView = workoutList.length <= 7;
     double screenWidth = MediaQuery.of(context).size.width;
     int crossAxisCount = screenWidth > 800 ? 2 : 1;
-    return Container(
-      margin: const EdgeInsets.only(top: 20),
-      child: GridView.builder(
-        itemCount: workoutList.length, // Number of items
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount:
-              crossAxisCount, // 1 for narrow screens, 2 for wider screens
-          childAspectRatio:
-              2, // Adjust based on your item's content and desired aspect ratio
-        ),
-        itemBuilder: (context, index) {
-          return WorkoutBox(
-            boxTitle: workoutList[index].title,
-            boxDescription: workoutList[index].description,
-            isSelectWorkoutView: isSelectView,
-            index: index, // THIS IS GIVING INCORRECT INDEX WHEN IN WORKOUT VIEW
-          ); // Your custom widget
-        },
+    return GridView.builder(
+      itemCount: workoutList.length, // Number of items
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount:
+            crossAxisCount, // 1 for narrow screens, 2 for wider screens
+        childAspectRatio:
+            2, // Adjust based on your item's content and desired aspect ratio
       ),
+      itemBuilder: (context, index) {
+        return WorkoutBox(
+          boxTitle: workoutList[index].title,
+          boxDescription: workoutList[index].description,
+          isSelectWorkoutView: isSelectView,
+          index: index, // THIS IS GIVING INCORRECT INDEX WHEN IN WORKOUT VIEW
+        ); // Your custom widget
+      },
     );
   }
 }
